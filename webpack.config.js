cacf317c7cd7b6be68b57c72ed40e4fcee836e64
Goph <tealeverywhere@gmail.com>
2023-08-10 22:38:46 -1000

    Initial dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/main.js', 
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
   module: {
